bf3bc290f12403079a69685d407e27de1b4f0445
BnjMui <142273088+BnjMui@users.noreply.github.com>
2024-04-03 19:50:04 +0200

    Fikset average rating til en desimal

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -13,7 +13,7 @@ return(
             <h3>"{element.title}"</h3>
             <p>Forfatter: {element.author_name}</p>
             <p>Første publikasjon: {element.first_publish_year}</p>
-            <p>Rating: {element.ratings_average}</p>
+            <p>Rating: {element.ratings_average ? (element.ratings_average).toFixed(1): "Rating not available"}</p>
             {element.isbn? <p>ISBN:{element.isbn.length===13? element.isbn[0]: element.isbn[1]}</p> : <p>ISBN ikke tilgjengelig</p>}
             <a target="_blank" href={`https://www.amazon.com/s?k=${element.isbn? element.isbn[0] : false}`}>Finn på Amazon</a>
           </article>
